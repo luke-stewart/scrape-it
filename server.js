92d0bf5b9fee8a0df35d018da80158dcc1bc92d0
Luke Stewart <lukestewart@gmail.com>
2018-03-19 22:03:24 -0700

    tweaks

--- server.js
+++ server.js
@@ -36,9 +36,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.Promise = Promise;
 
-// mongoose.connect(MONGODB_URI,  {
-//   	useMongoClient: true
-// });
+mongoose.connect(MONGODB_URI);
 
 var db = mongoose.connection;
 
